db82648bbf0c32669ba5674bb51ac7298e591e00
DotNetCraft <dotnetcraft@gmail.com>
2024-06-12 20:53:33 +1000

    Added a few tests to setup + some refactoring

--- src/DotNetCraft.Common.Databases.Abstractions/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DotNetCraft.Common.Databases.Abstractions
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ tests/DotNetCraft.Common.Databases.Abstractions.Tests/ProjectionDefinitionTests.cs
@@ -0,0 +1,40 @@
+using DotNetCraft.Common.Databases.Abstractions.Options;
+using System.Linq.Expressions;
+
+namespace DotNetCraft.Common.Databases.Abstractions.Tests
+{
+    [TestClass]
+    public class ProjectionDefinitionTests
+    {
+        private class SimpleEntity
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
+        [TestMethod]
+        public void ProjectionDefinitionIncludeTest()
+        {
+            var projection = new ProjectionDefinition<SimpleEntity>();
+            Expression<Func<SimpleEntity, object>> field = entity => entity.Name;
+            projection.Include(field);
+
+            Assert.AreEqual(0, projection.Excludes.Count);
+            Assert.AreEqual(1, projection.Includes.Count);
+            Assert.AreEqual(field, projection.Includes[0]);
+        }
+
+        [TestMethod]
+        public void ProjectionDefinitionExcludeTest()
+        {
+            var projection = new ProjectionDefinition<SimpleEntity>();
+            Expression<Func<SimpleEntity, object>> field = entity => entity.Name;
+            projection.Exclude(field);
+
+            Assert.AreEqual(0, projection.Includes.Count);
+            Assert.AreEqual(1, projection.Excludes.Count);
+            Assert.AreEqual(field, projection.Excludes[0]);
+        }
+    }
+}
