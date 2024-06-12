2ac8a35eac297474c140641293cb63401ca03ec8
DotNetCraft <dotnetcraft@gmail.com>
2024-06-12 20:58:35 +1000

    just test

--- tests/DotNetCraft.Common.Databases.Abstractions.Tests/ProjectionDefinitionTests.cs
+++ tests/DotNetCraft.Common.Databases.Abstractions.Tests/ProjectionDefinitionTests.cs
@@ -10,7 +10,6 @@ namespace DotNetCraft.Common.Databases.Abstractions.Tests
         {
             public int Id { get; set; }
             public string Name { get; set; }
-            public string Description { get; set; }
         }
 
         [TestMethod]
